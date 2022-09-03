574587dc248df67a89a8fd42fa770ae0450091a3
Dicrhoic <88888289+Dicrhoic@users.noreply.github.com>
2022-09-03 21:34:29 +1000

    2 files modified

--- src/app/nav-body/nav-body.component.ts
+++ src/app/nav-body/nav-body.component.ts
@@ -1,13 +1,18 @@
 import { Component} from '@angular/core';
+declare var require: any
 
 @Component({
   selector: 'app-nav-body',
   templateUrl: './nav-body.component.html',
   styleUrls: ['./nav-body.component.css'],
 })
+
+
 export class NavBodyComponent {
+  
  readFile()
  {
+   
   var fs = require('fs'),
   xml2js = require('xml2js');
 
