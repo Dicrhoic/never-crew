b6181501cca9b25e81cf3bff3ac0812b9a134481
Dicrhoic <88888289+Dicrhoic@users.noreply.github.com>
2022-09-03 21:56:56 +1000

    2 files modified

--- src/app/nav-body/nav-body.component.ts
+++ src/app/nav-body/nav-body.component.ts
@@ -1,27 +1,26 @@
-import { Component} from '@angular/core';
-declare var require: any
+import { Component } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'app-nav-body',
   templateUrl: './nav-body.component.html',
   styleUrls: ['./nav-body.component.css'],
 })
-
-
 export class NavBodyComponent {
-  
- readFile()
- {
-   
- // var fs = require('fs'),
-  //xml2js = require('xml2js');
+  readFile() {
+    var fs = require('fs'),
+      xml2js = require('xml2js');
 
- var parser = new xml2js.Parser();
- fs.readFile("/assests" + '/SSRCharacters.xml', function(err, data) {
-  parser.parseString(data, function (err, result) {
-      console.dir(result);
-      console.log('Done');
-  });
-});
- }
+    var parser = new xml2js.Parser();
+    fs.readFile('/db/SSRCharacters.xml', function (err, data) {
+      parser.parseString(data, function (err, result) {
+        console.dir(result);
+        console.log('Done');
+      });
+    });
+  }
+  loadCharacterDropdown()
+  {
+    this.readFile();
+  }
 }
