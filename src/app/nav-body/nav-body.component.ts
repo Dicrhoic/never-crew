b4566ea6eaa10f3151f991207ae10382860d59ef
Dicrhoic <88888289+Dicrhoic@users.noreply.github.com>
2022-09-04 22:27:30 +1000

    2 files modified

--- src/app/nav-body/nav-body.component.ts
+++ src/app/nav-body/nav-body.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 import characters from '../db/SSRCharacters.json';
 declare var require: any;
 
@@ -9,30 +9,34 @@ declare var require: any;
   styleUrls: ['./nav-body.component.css'],
 })
 export class NavBodyComponent {
+  characterData: any;
+
+  constructor() {
+    this.characterData = characters;
+  }
 
   readFile() {
-    var __dirname = "./";
-    var fileName = (__dirname + 'test.xml');
+    console.log();
+    var __dirname = './';
+    var fileName = __dirname + 'test.xml';
     var fs = require('fs'),
       xml2js = require('xml2js');
-      var parser = new xml2js.Parser();
-      console.log('index', characters);
-      fs.readFile(__dirname + 'nav-body.component.css', function(err, data) {
-        if(err)
-        { 
-          console.log(err);
-        }
-        if (!fs.exists(fileName)) {
-         console.log("File does not exist");
-        }
-          parser.parseString(data, function (err, result) {
-              console.dir(result);
-              console.log('Done');
-          });
+    var parser = new xml2js.Parser();
+    console.log('index', characters);
+    fs.readFile(__dirname + 'nav-body.component.css', function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      if (!fs.exists(fileName)) {
+        console.log('File does not exist');
+      }
+      parser.parseString(data, function (err, result) {
+        console.dir(result);
+        console.log('Done');
       });
+    });
   }
-  loadCharacterDropdown()
-  {
+  loadCharacterDropdown() {
     this.readFile();
   }
 }
